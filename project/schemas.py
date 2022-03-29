ef0eacd930f102c153958b1e53ade474315257fb
Marcos Vzq <marcos.sis90@gmail.com>
2022-03-28 19:01:13 -0600

    added validations to model user

--- project/schemas.py
+++ project/schemas.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
+from pydantic import validator
 
 class UserBaseModel(BaseModel):
     username: str
     password: str
-    
+
+    @validator('username')
+    def username_validator(cls, username):
+        if len(username) < 3 or len(username) > 50:
+            raise ValueError('La longitud debe encontrarse entre 3 y 50 caracteres')
+
+        return username
